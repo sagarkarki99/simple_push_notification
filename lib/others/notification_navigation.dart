68cc1f7d3a28fd418a23b0cf46b73380072d027d
sagarkarki99 <sagarkarki184@gmail.com>
2022-07-25 22:32:17 +0545

    recieving navigatorKey for notification navigation

--- lib/others/notification_navigation.dart
+++ lib/others/notification_navigation.dart
@@ -1,5 +1,18 @@
+import 'package:flutter/material.dart';
+
 import 'notification_payload.dart';
 
 abstract class NotificationNavigation {
   void navigateTo(NotificationPayload payload);
+}
+
+class DefaultNotificationNavigation implements NotificationNavigation {
+  final GlobalKey<NavigatorState> navigatorKey;
+
+  DefaultNotificationNavigation({required this.navigatorKey});
+
+  @override
+  void navigateTo(NotificationPayload payload) {
+    payload.trigger(navigatorKey.currentContext!);
+  }
 }
